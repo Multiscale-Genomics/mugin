82871413595a48959e9036f70f7b3c9a2b818647
Marco Pasi <mfpasi@gmail.com>
2016-05-20 15:31:24 +0100

    v0.2: directional, double links and code cleanup

--- js/mugin.js
+++ js/mugin.js
@@ -1,5 +1,5 @@
 /*
- * datagraph.js 
+ * mugin.js 
  *
  *  Copyright (C) 2016 Marco Pasi <mf.pasi@gmail.com> 
  *
@@ -9,18 +9,165 @@
  *  (at your option) any later version.
  *
  * v0.1 160518
+ * v0.2 160520
+ * TODO: 
+ *  1. directional links DONE v0.2
+ *  2. double links      DONE v0.2
+ *  3. improved metadata
+ *  4. editable metadata
+ *  5. graph bidimensional sorting
+ *  6. multiple entries per link
  *
  */
 
+/* Constants */
+var PI = 3.1415;
+
+var NTYPES = 3,
+    TYPE_TODO = 0,
+    TYPE_DONE = 1,
+    TYPE_MAYBE= 2,
+    TYPE_CLASS=["todo", "done", "maybe"];
+
+/* Configuration */
 var width = 600,
     height = 500,
     transitionDuration = 1000, // ms
-    svg, graph = {}, force;
+    link_dist = width/4.0,
+    link_spread = 0.2; // radians
+
+/* Globals */
+var svg, graph = {}, force;
+
+
+/* General utility functions */
+Array.prototype.findAll = function(searchElement) {
+    var indices = [];
+    var idx = this.indexOf(searchElement);
+    while (idx != -1) {
+        indices.push(idx);
+        idx = this.indexOf(searchElement, idx + 1);
+    }
+    return indices;
+}
 
 function capitalize(string) {
     return string.substr(0, 1).toUpperCase() + string.substr(1);
 }
 
+
+/* Other utility functions */
+function addnode(graph, nodename) {
+    /* XXX move to the graph prototype.
+     * Add a node to the graph 
+     */
+
+    if(graph.nodelist.indexOf(nodename) >= 0)
+        return graph.nodelist.indexOf(nodename);
+    
+    var ret = graph.nodelist.push(nodename);
+    var th  = Math.random() * 2 * PI;
+    graph.nodes.push({
+        "x": (1+Math.sin(th))*width/2,
+        "y": (1+Math.cos(th))*width/2,
+        "rx": Math.max(30, nodename.length*4),
+        "ry": 30,
+        "name": nodename});
+    return ret-1;
+}
+
+function correctLink(d, dth) {
+    /* 
+     * Correct link positions according to node radii.
+     * dth is the minimum angular distance between links (in radians).
+     *
+     * Expects nodes to be ellipses.
+     */
+    var rdx = d.target.x - d.source.x,
+        rdy = d.target.y - d.source.y,
+        th  = Math.atan2(rdy, rdx),
+        dsx = d.source.rx * Math.cos(th-dth/2),
+        dsy = d.source.ry * Math.sin(th-dth/2),
+        dtx = d.target.rx * Math.cos(th+dth/2),
+        dty = d.target.ry * Math.sin(th+dth/2),
+        dx  = rdx - dsx - dtx,
+        dy  = rdy - dsy - dty;
+    
+    return {
+        x0 : d.source.x + dsx,
+        y0 : d.source.y + dsy,
+        x1 : d.target.x - dtx,
+        y1 : d.target.y - dty,
+        dx : dx,
+        dy : dy,
+        dr : Math.sqrt(dx * dx + dy * dy)
+    };
+}
+
+function pathline(link) {
+    // A line from (x0,y0) to (x1,y1).
+    return "M" + link.x0 + "," + link.y0 +
+        "L" + link.x1 + "," + link.y1;
+}
+
+function patharc(link) {
+    // A circular arc of radius dr from (x0,y0) to (x1,y1).
+    return "M" + link.x0 + "," + link.y0 +
+        "A" + link.dr + "," + link.dr + " 0 0,1 " +
+        link.x1 + "," + link.y1;
+}
+
+function textifynode(d) {
+    // Return text description of node
+    return "<h3>"+d.name+"</h3>";
+}
+
+function textifylink(d) {
+    // Return text description of link
+    return "<h3>"+d.source.name+"&mdash;"+d.target.name+"</h3><dl>"+
+        "<dd>Description: " + d.description + "</dd>"+
+        "<dd>Reference: " + d.reference + "</dd>"+
+        "<dd>Tools: " + d.tools + "</dd>"+
+        "</dl>";
+}
+
+function make_markers() {
+    // Create required markers for arrows
+    var defs = svg.append("defs");
+    var mkw = 3.0;
+    for(t=0; t<NTYPES; t++) {
+        defs.append("marker")
+	.attr({
+	    "id":"rarrow"+t,
+	    "viewBox":"0 -5 10 10",
+	    "refX":8,
+	    "refY":0,
+	    "markerWidth":mkw,
+	    "markerHeight":mkw,
+	    "orient":"auto"
+	})
+	.append("path")
+	.attr("d", "M0,-5L10,0L0,5")
+	.attr("class","arrowHead "+TYPE_CLASS[t]);
+    
+        defs.append("marker")
+	.attr({
+	    "id":"larrow"+t,
+	    "viewBox":"0 -5 10 10",
+	    "refX":0,
+	    "refY":0,
+	    "markerWidth":mkw,
+	    "markerHeight":mkw,
+	    "orient":"auto"
+	})
+	.append("path")
+	.attr("d", "M10,-5L0,0L10,5")
+	.attr("class","arrowHead "+TYPE_CLASS[t]);
+    }
+}
+
+
+/* Main Datagraph structure */
 // XXX Use the function.call(instance) pattern
 //     to wrap all this into an object with an
 //     assigned id, tsv; then the _this pattern
@@ -29,20 +176,36 @@ function datagraph(id, tsv) {
     svg = d3.select(id).append("svg")
         .attr("width",  width)
         .attr("height", height);
+
+    make_markers();
     
-    graph.nodelist = [];
+    graph.nodelist = []; // temporary structure to accumulate nodes
+    graph.linklist = []; // temporary structure to count links
     graph.nodes = [];
     graph.links = [];
         
     d3.tsv(
         tsv,
         function(d) {
-            if(graph.nodelist.indexOf(d.data1) == -1)
-                graph.nodelist.push(d.data1);
-            if(graph.nodelist.indexOf(d.data2) == -1)
-                graph.nodelist.push(d.data2);
-            var src = graph.nodelist.indexOf(d.data1);
-            var trg = graph.nodelist.indexOf(d.data2);
+            // Add nodes
+            var src = addnode(graph, d.data1),
+                trg = addnode(graph, d.data2);
+            
+            // Check if link exists
+            var linkid = Math.min(src,trg)*1e10+Math.max(src,trg),
+                linkis = graph.linklist.findAll(linkid),
+                weight = 1;
+            graph.linklist.push(linkid);
+            if(linkis.length > 0) {
+                weight = graph.links[linkis[0]].weight + 1;
+                if(weight > 3) // Max 3 links
+                    return -1; // skip
+                linkis.forEach(function(l) {
+                    graph.links[l].weight = weight;
+                });
+            }
+            
+            // Compute derived variables
             var flow= 1;
             if(d.flow == "<-") {
                 var tmp = src;
@@ -51,6 +214,18 @@ function datagraph(id, tsv) {
             }else if(d.flow == "<->") {
                 flow = 2;
             }
+            
+            /* Types:
+             *  1. available tools: done
+             *  2. demonstrated to be relevant, but no available tools: todo
+             *  3. potentially relevant: maybe
+             */  
+            var type = TYPE_TODO;
+            if(d.reference == "XXX")
+                type = TYPE_MAYBE;
+            else if(d.tools != "")
+                type = TYPE_DONE;            
+            
             graph.links.push({
                 "source": src,
                 "target": trg,
@@ -58,23 +233,16 @@ function datagraph(id, tsv) {
                 "description": d.description,
                 "reference": d.reference,
                 "notes": d.notes,
-                "tools": d.tools
+                "tools": d.tools,
+                "type": type,
+                "weight": weight
             });
         },
         // Build the UI
         function(error, data) {
-            // Make nodes from nodelist
-            graph.nodelist.forEach(
-                function(d, i, a) {
-                    var j = Math.random() * a.length;
-                    graph.nodes.push({
-                        "x":(1+Math.sin(j*6.28/a.length))*width/2,
-                        "y":(1+Math.cos(j*6.28/a.length))*width/2,
-                        "name":d});
-                });
             // Create layout
             force = d3.layout.force()
-                .linkDistance(width/4.0)
+                .linkDistance(link_dist)
                 .linkStrength(.02)
                 .charge(-120)
                 .gravity(.015)
@@ -85,6 +253,8 @@ function datagraph(id, tsv) {
     return this;
 }
 
+/* Main update function */
+
 function update() {
     force
         .nodes(graph.nodes)
@@ -93,64 +263,89 @@ function update() {
     // Add/update links
     var link = svg.selectAll('.link')
         .data(graph.links);
+    
     link.exit().remove();
-    var linkEnter = link.enter().append('line')
+    
+    var linkEnter = link.enter().append('path')
         .attr('class', function(d) {
-            var cl = "link";
-            if(d.reference == "XXX")
-                cl += " new";
-            else if(d.tools != "") {
-                cl += " old";
-            }
-            return cl;
+            return "link " + TYPE_CLASS[d.type];
         })
+        .attr({
+            "marker-end":   function(d) {
+                return "url(#rarrow"+d.type+")";
+            },
+            "marker-start": function(d) {
+                if(d.flow == 2)
+                    return "url(#larrow"+d.type+")";
+            }})
         .on("click", clicklink);
-    linkEnter.append("marker")
-	.attr({
-	    "id":"arrow",
-	    "viewBox":"0 -5 10 10",
-	    "refX":5,
-	    "refY":0,
-	    "markerWidth":4,
-	    "markerHeight":4,
-	    "orient":"auto"
-	})
-        .append("path")
-	.attr("d", "M0,-5L10,0L0,5")
-        .attr("class","arrow");
+
+    /* Ideally this is done with positional markers;
+     * tick() updates their position given the direction.
+     * See https://www.w3.org/TR/svg-markers/.
+     * 
+     *linkEnter.append('marker')
+     *        .attr({
+     *         'href': 'url(#rarrow)',
+     *         'position': '90%'});
+     */
     
     // Add/update nodes
     var node = svg.selectAll('.node')
         .data(graph.nodes);
+    
     node.exit().remove();
+    
     var nodeEnter = node.enter().append('g')
         .attr('class', 'node')
         .on("click", clicknode)
         .call(force.drag);
-    nodeEnter.append("ellipse")
-        .attr("rx", function(d) {
-            return Math.max(30, d.name.length*4);
-        })
-        .attr("ry", 30);
-    nodeEnter.append("text")
-        .attr("dy", 5)
-        .attr("text-anchor", "middle")
-        .attr("class", "nodetxt")
+    
+    nodeEnter.append("ellipse") // Add ellipse
+        .attr({
+            "rx": function(d) {return d.rx;},
+            "ry": function(d) {return d.ry;}});
+    
+    nodeEnter.append("text")    // Add text
+        .attr({
+            "dy": 5,
+            "text-anchor": "middle",
+            "class": "nodetxt"})
         .text(function(d) { return d.name; });
 }
 
+/* Main timestep function */
+
 function tick() {
     var link = svg.selectAll('.link');
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
+    link.attr("d", function(d) {
+        var clink = correctLink(d, link_spread);
+        if(d.weight == 1 || d.flow == 2) 
+            return pathline(clink);
+        else
+            return patharc(clink);
+    })
+        .style("opacity", function(d) {
+            var clink = correctLink(d, link_spread),
+                invmindist = 0.03; // 3/link_dist
+            // if(d.tools=="cgDNA")
+            //     $("#debug").text(
+            //         clink.x0.toFixed(1)+","+clink.y0.toFixed(1)+","+
+            //         clink.x1.toFixed(1)+","+clink.y1.toFixed(1)+","+
+            //         clink.dx.toFixed(1)+","+clink.dy.toFixed(1)+","+
+            //         clink.dr.toFixed(1)+","+(invmindist*clink.dr).toFixed(1));
+            return Math.min(1.0, invmindist*clink.dr);
+        });
+    
     var node = svg.selectAll('.node');
     node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 }
 
+
+/* Callback functions */
+
 function clicknode(d) {
+    console.log(d);
     if (d3.event.defaultPrevented) return; // ignore drag
     $("#selector-hp").html(textifynode(d));
     if (d3.event.shiftKey) {
@@ -164,17 +359,6 @@ function clicknode(d) {
 }
 
 function clicklink(d) {
+    console.log(d);
     $("#selector-hp").html(textifylink(d));
-}
-
-function textifynode(d) {
-    return "<h3>"+d.name+"</h3>";
-}
-
-function textifylink(d) {
-    return "<h3>"+d.source.name+"&mdash;"+d.target.name+"</h3><dl>"+
-        "<dd>Description: " + d.description + "</dd>"+
-        "<dd>Reference: " + d.reference + "</dd>"+
-        "<dd>Tools: " + d.tools + "</dd>"+
-        "</dl>";
 }
